2654cec981dcec37c7e1ad2d28d3db653f6bd28e
Imani Ward <idward712@gmail.com>
2023-02-09 14:16:34 -0500

    o

--- Who am I/ViewController.swift
+++ Who am I/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var image: UIImageView!
+    
+    @IBOutlet var points: UILabel!
+    
+    @IBOutlet var guess: UITextField!
+    
+    @IBOutlet var info: UILabel!
+    
+    
+    var person = people[0]
+    
+    var point = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updatePoint()
+        nextPerson()
     }
-
-
+    
+    
+    func rando() -> Int{
+        return Int.random(in:0...people.count - 1)
+    }
+    
+    func nextPerson(){
+        let x = rando()
+        person = people.remove(at: x)
+        image.image = person.image
+        info.text = person.description
+        
+        
+    }
+    func updatePoint(){
+        points.text = String(point)
+    }
+    
+    @IBAction func answer(_ sender: Any) {
+        guard !people.isEmpty else{return}
+        if let name = guess.text{
+            if name.lowercased() == person.name.lowercased(){
+                point += 1
+            }
+        }
+        updatePoint()
+        nextPerson()
+    }
+    
 }
-
+struct Person{
+    var image:UIImage
+    var name: String
+    var description:String
+    
+}
+var people = [Person(image:UIImage(named: "image1")!, name: "Lauryn Hill", description: ""), Person(image:UIImage(named: "")!, name: "", description: ""), Person(image:UIImage(named: "")!, name: "", description: ""), Person(image:UIImage(named: "")!, name: "", description: ""), Person(image:UIImage(named: "")!, name: "", description: "")]
